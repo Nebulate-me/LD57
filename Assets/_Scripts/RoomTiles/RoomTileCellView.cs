7a4283f536f92442bcb824cb8ea2376713134d31
Firellon <macfirellon@gmail.com>
2025-07-22 20:11:48 +0200

    feat: introduce doors

--- Assets/_Scripts/RoomTiles/RoomTileCellView.cs
+++ Assets/_Scripts/RoomTiles/RoomTileCellView.cs
@@ -1,4 +1,6 @@
+using System;
 using _Scripts.Rooms;
+using Plugins.Sirenix.Odin_Inspector.Modules;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +10,26 @@ namespace _Scripts.RoomTiles
     {
         [SerializeField] private RectTransform tileTransform;
         [SerializeField] private Image tileImage;
+        [SerializeField] private RoomDirectionToGameObjectDictionary doorObjects;
         public void SetUp(RoomTileCell roomTileCell)
         {
             tileImage.sprite = roomTileCell.Tile.UsedSprite;
             tileTransform.rotation = roomTileCell.Direction.ToRotation();
+            foreach (var doorObject in doorObjects.Values)
+            {
+                doorObject.SetActive(false);
+            }
+            foreach (var tileDoorDirection in roomTileCell.Tile.DoorDirections)
+            {
+                doorObjects[tileDoorDirection].SetActive(true);
+            }
         }
+    }
+
+
+    [Serializable]
+    public class RoomDirectionToGameObjectDictionary : UnitySerializedDictionary<RoomDirection, GameObject>
+    {
+        
     }
 }
