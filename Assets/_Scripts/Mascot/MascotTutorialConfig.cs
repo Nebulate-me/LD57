7ddb1922ca3b24ad0544be1185c334eb52543b2f
Firellon <macfirellon@gmail.com>
2025-09-21 14:45:04 +0200

    fix: update tutorial config texts

--- Assets/_Scripts/Mascot/MascotTutorialConfig.cs
+++ Assets/_Scripts/Mascot/MascotTutorialConfig.cs
@@ -6,7 +6,7 @@ namespace _Scripts.Mascot
     [CreateAssetMenu(menuName = "LD57/Create MascotTutorialConfig", fileName = "MascotTutorialConfig", order = 0)]
     public class MascotTutorialConfig : ScriptableObject
     {
-        [SerializeField] private List<MascotTutorialStepConfig> steps =  new List<MascotTutorialStepConfig>();
+        [SerializeField] private List<MascotTutorialStepConfig> steps = new();
         
         public List<MascotTutorialStepConfig> Steps => steps;
     }
--- Assets/_Scripts/Mascot/MascotTutorialStepConfig.cs
+++ Assets/_Scripts/Mascot/MascotTutorialStepConfig.cs
@@ -1,4 +1,5 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace _Scripts.Mascot
@@ -8,10 +9,36 @@ namespace _Scripts.Mascot
     {
         [SerializeField, TextArea(2, 5)] private string phrase = string.Empty;
         [SerializeField] private MascotTutorialTargetType targetType;
+        [ShowIf(nameof(IsTargetTopPanel)), SerializeField] private MascotTutorialTopPanelTargetType topPanelTargetType;
+        [ShowIf(nameof(IsTargetBottomPanel)), SerializeField] private MascotTutorialBottomPanelTargetType bottomPanelTargetType;
+
         [SerializeField] private MascotTutorialActionType actionType;
+        
+        private bool IsTargetTopPanel => targetType == MascotTutorialTargetType.TopPanel;
+        private bool IsTargetBottomPanel => targetType == MascotTutorialTargetType.BottomPanel;
         
         public string Phrase => phrase;
         public MascotTutorialTargetType TargetType => targetType;
         public MascotTutorialActionType ActionType => actionType;
+    }
+
+    public enum MascotTutorialTopPanelTargetType
+    {
+        None = 0,
+        Timer = 1,
+        MissionCards = 2,
+        MissionCardName = 3,
+        MissionCardRoomIcons = 4,
+        MissionCardRoomWindows = 5,
+        MissionCardScore = 6,
+        Score = 7,
+        MissionCounter = 8,
+    }
+    
+    public enum MascotTutorialBottomPanelTargetType
+    {
+        None = 0,
+        RoomCards = 1,
+        RedrawCardsButton = 2,
     }
 }
